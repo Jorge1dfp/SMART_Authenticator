e74802aa21cd97e6fc9f3ef577526d0b38b499de
Jorge1dfp <35082694+Jorge1dfp@users.noreply.github.com>
2019-05-09 11:43:25 -0400

    Face Dectection
    To be honest only got the program to detect the front facing test

--- SMART_Authenticator.py
+++ SMART_Authenticator.py
@@ -1,12 +1,20 @@
 import cv2
 import numpy as np
 
+face_cascade = cv2.CascadeClassifier(r"C:\Users\Jorge Flores\PycharmProjects\SMART_Authenticator\haarcascade_frontalface_default.xml")
+
 cap = cv2.VideoCapture(0)
 
 while True:
     ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        roi_color = frame[y:y + h, x:x + w]
 
     cv2.imshow('frame', frame)
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         cap.release()
         cv2.destroyAllWindows()
